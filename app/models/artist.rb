0ed92f74ff14ec1a4bf37f44cbef8fc7cf064d51
Rene Garcia <rene@generalui.com>
2015-04-18 16:23:13 -0700

    adding avatar en changing other stuff

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,11 @@
 class Artist < ActiveRecord::Base
+	mount_uploader :avatar, AvatarUploader
 	mount_uploader :portafolio_1, PortafolioUploader
 	mount_uploader :portafolio_2, PortafolioUploader
 	mount_uploader :portafolio_3, PortafolioUploader
+	validates :email, :nickname, :portafolio_1, presence: true
+
+	def avatar_picture
+		self.avatar.blank? ? "default-avatar.png" : self.avatar
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418220811) do
+ActiveRecord::Schema.define(version: 20150418224740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150418220811) do
     t.string   "portafolio_3"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "avatar"
   end
 
   create_table "blank_walls", force: :cascade do |t|
